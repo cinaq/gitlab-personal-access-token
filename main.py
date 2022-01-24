ab259c9d072f0cf2bfc676093d55c8d0d5384383
Xiwen Cheng <x@cinaq.com>
2022-01-24 17:20:44 +0100

    Name column is required

--- main.py
+++ main.py
@@ -22,13 +22,17 @@ pg_password = os.environ.get('PG_PASSWORD')
 api_key = os.environ.get('API_KEY')
 user_id = int(os.environ.get('USER_ID', 1))
 user_scopes = os.environ.get('USER_SCOPES', default_scopes)
+api_name = os.environ.get('API_NAME', "Managed by CINAQ gitlab-personal-access-token")
 
 token_digest = base64.b64encode(hashlib.sha256((api_key + db_key_base[:32]).encode('utf-8')).digest()).decode('utf-8')
 
 
 def get_id(conn, user_id, token_digest):
     with conn.cursor() as cursor:
-        cursor.execute("""SELECT id FROM personal_access_tokens WHERE user_id = %s and token_digest = %s""", (user_id, token_digest,))
+        cursor.execute(
+            """SELECT id FROM personal_access_tokens WHERE user_id = %s and token_digest = %s""",
+            (user_id, token_digest,)
+        )
         row = cursor.fetchone()
         if row:
             return row[0]
@@ -37,7 +41,12 @@ def get_id(conn, user_id, token_digest):
 
 def create_pat(conn, user_id, user_scopes, token_digest):
     with conn.cursor() as cursor:
-        cursor.execute("""INSERT INTO personal_access_tokens (impersonation, scopes, revoked, user_id, token_digest) VALUES  (%s, %s, %s, %s, %s)""", (False, user_scopes, False, user_id, token_digest))
+        cursor.execute(
+            """INSERT INTO personal_access_tokens 
+                (name, impersonation, scopes, revoked, user_id, token_digest)
+                VALUES  (%s, %s, %s, %s, %s, %s)""",
+            (api_name, False, user_scopes, False, user_id, token_digest)
+        )
 
 connect_str = f"dbname='{pg_dbname}' user='{pg_username}' host='{pg_host}' password='{pg_password}' port='{pg_port}'"
 conn = psycopg2.connect(connect_str)
