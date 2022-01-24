3263f7f5e7dcaa3c986520a57b09f8c0b9f5161f
Xiwen Cheng <x@cinaq.com>
2022-01-24 17:14:08 +0100

    Fix column name and minor refactoring

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import hashlib
 import psycopg2
 import os
 
-default_scope = """---
+default_scopes = """---
 - api
 - read_user
 - read_api
@@ -21,7 +21,7 @@ pg_username = os.environ.get('PG_USERNAME')
 pg_password = os.environ.get('PG_PASSWORD')
 api_key = os.environ.get('API_KEY')
 user_id = int(os.environ.get('USER_ID', 1))
-user_scope = os.environ.get('USER_SCOPE', default_scope)
+user_scopes = os.environ.get('USER_SCOPES', default_scopes)
 
 token_digest = base64.b64encode(hashlib.sha256((api_key + db_key_base[:32]).encode('utf-8')).digest()).decode('utf-8')
 
@@ -35,16 +35,16 @@ def get_id(conn, user_id, token_digest):
         else:
             return None
 
-def create_pat(conn, user_id, user_scope, token_digest):
+def create_pat(conn, user_id, user_scopes, token_digest):
     with conn.cursor() as cursor:
-        cursor.execute("""INSERT INTO personal_access_tokens (impersonation, scope, revoked, user_id, token_digest) VALUES  (%s, %s, %s, %s, %s)""", (False, user_scope, False, user_id, token_digest))
+        cursor.execute("""INSERT INTO personal_access_tokens (impersonation, scopes, revoked, user_id, token_digest) VALUES  (%s, %s, %s, %s, %s)""", (False, user_scopes, False, user_id, token_digest))
 
 connect_str = f"dbname='{pg_dbname}' user='{pg_username}' host='{pg_host}' password='{pg_password}' port='{pg_port}'"
 conn = psycopg2.connect(connect_str)
 
 current_id = get_id(conn, user_id, token_digest)
 if not current_id:
-    create_pat(conn, user_id, user_scope, token_digest)
+    create_pat(conn, user_id, user_scopes, token_digest)
 new_id = get_id(conn, user_id, token_digest)
 if not new_id:
     raise Exception("Failed to create PAT")
